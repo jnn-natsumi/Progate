5e4097eb19bede6a468638ed011131ca2b1d5176
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-11-18 00:08:34 +0900

    Javascript prpgate

--- JavaScript/dog/11-17/11-17.js
+++ JavaScript/dog/11-17/11-17.js
@@ -1,6 +1,17 @@
 'use strict';
 
-myMes =  new  Array(3);
+
+// Step テーマ  Array() 配列
+var myMes =  new  Array(3);  // 変数myMesは3個の「行」を持つことになる。
 myMes[0] = "hello";
 myMes[1] = "nice to meet you";
 myMes[2] = "my  name is haru";
+
+document.write(myMes[2]);
+console.log(myMes)
+
+function hello() {
+    console.log("hello?");
+}
+
+hello();
